cd208acdb730f08d14c1ac3b69d6136794d20fdf
reignsystemsai <drteventbrite@gmail.com>
2026-08-17 05:37:25 -0400

    build-91-2 add migration-free Translator Call coordinator

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const path = require("path");
+const { router: translatorCallsRouter } = require("./src/server/translator/TranslatorCalls");
 require("dotenv").config();
 
 const app = express();
@@ -20,6 +21,7 @@ app.use(
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/api/translator-calls", translatorCallsRouter);
 
 app.get("/health", (req, res) => {
   res.status(200).json({
